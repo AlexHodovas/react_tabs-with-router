15ac8a960fc9f836054dfd95566911f6c4a76df7
AlexHodovas <o.hodovas@gmail.com>
2019-12-24 17:52:17 +0200

    Updates

---
+++ precache-manifest.7b59663363fafb8bf7f18dabd1cb37d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028893506013c8f03e16a9f04b71ba19",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "0f5e91ccefa8d7ed74c0",
+    "url": "/react_tabs-with-router/static/css/main.5edb1af9.chunk.css"
+  },
+  {
+    "revision": "8c4f93ffb774790a8ff4",
+    "url": "/react_tabs-with-router/static/js/2.b066fd00.chunk.js"
+  },
+  {
+    "revision": "0f5e91ccefa8d7ed74c0",
+    "url": "/react_tabs-with-router/static/js/main.3a8a1da6.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
